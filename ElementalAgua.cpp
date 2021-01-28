651d07672ce35fa35948dd29c5824592af4de40b
iagocharon <icharon@fi.uba.ar>
2021-01-28 09:43:47 -0300

    Agregue el metodo ataque

--- ElementalAgua.cpp
+++ ElementalAgua.cpp
@@ -1,6 +1,6 @@
 #include "ElementalAgua.h"
 
-ElementalAgua::ElementalAgua(string nombre, string elemento, int escudo, int vida) : Personaje(nombre, elemento, escudo, vida) {
+ElementalAgua::ElementalAgua(string nombre, string elemento, int escudo, float vida) : Personaje(nombre, elemento, escudo, vida) {
     this->vecesAlimentado = 0;
 }
 
@@ -19,5 +19,42 @@ void ElementalAgua::alimentar() {
     }
     else {
         cout << "Ya no hay plancton suficiente para alimentar de nuevo al personaje" << endl;
+    }
+}
+
+void ElementalAgua::atacar(Personaje* enemigos[MAX_PERSONAJES]) {
+    cout << "ATAQUE DE ELEMENTAL DE AGUA" << endl;
+    int filaObjetivo, columnaObjetivo;
+    cout << "\tIngrese las coordenadas del destino del ataque: " << endl;
+    cout << "\t\tFila: ";
+    cin >> filaObjetivo;
+    cout << "\t\tColumna: ";
+    cin >> columnaObjetivo;
+    
+    for (int i = 0; i < MAX_PERSONAJES; i++) {
+        if ((enemigos[i]->getFila() == filaObjetivo) && (enemigos[i]->getColumna() == columnaObjetivo)) {
+            if (this->energia > MINIMO_ENERGIA_AGUA) {
+                float danio = DANIO_AGUA;
+                if (enemigos[i]->getElemento() == ELEMENTO_FUEGO) {
+                    danio += MODIFICADOR_AGUA;
+                }
+                else if (enemigos[i]->getElemento() == ELEMENTO_TIERRA) {
+                    danio -= MODIFICADOR_AGUA;
+                }
+                if (enemigos[i]->getEscudo() == 1) {
+                    danio = danio * 0.9;
+                }
+                else if (enemigos[i]->getEscudo() == 2) {
+                    danio = danio * 0.8;
+                }
+                else if (enemigos[i]->getEscudo() > 2) {
+                    danio = danio * 0.2;
+                }
+                enemigos[i]->setVida(enemigos[i]->getVida() - danio);
+            }
+            else {
+                cout << "\tNo tiene energia suficiente para atacar." << endl;
+            }
+        }
     }
 }
--- ElementalAgua.h
+++ ElementalAgua.h
@@ -3,11 +3,15 @@
 
 #include <iostream>
 #include "Personaje.h"
+#include <vector>
+
+#define AUMENTO_ENERGIA_AGUA 10
+#define TOPE_ALIMENTACIONES 3
+#define MINIMO_ENERGIA_AGUA 5
+#define DANIO_AGUA 20
+#define MODIFICADOR_AGUA 10
 
 using namespace std;
-
-const int AUMENTO_ENERGIA_AGUA = 10;
-const int TOPE_ALIMENTACIONES = 3;
 
 class ElementalAgua: public Personaje{
 private:
@@ -16,12 +20,14 @@ private:
 public:
     // PRE: Los parámetros recibidos son válidos
     // POS: Crea un ElementalAgua cargando sus atributos
-    ElementalAgua(string nombre, string elemento, int escudo, int vida);
+    ElementalAgua(string nombre, string elemento, int escudo, float vida);
 
     // PRE: -
     // POS: Notifica al usuario cómo fue alimentado el Personaje y aumenta la energía en AUMENTO_ENERGIA_AGUA
     //      En caso de ser efectuada la alimentación aumenta en 1 las vecesAlimentado
     void alimentar();
+    
+    void atacar(Personaje* enemigos[MAX_PERSONAJES]);
 };
 
 #endif //TP2_ELEMENTALAGUA_H
