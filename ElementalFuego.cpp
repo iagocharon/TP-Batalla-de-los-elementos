4b598757fc7c33ddc74784155bdf29d5ff904aec
iagocharon <icharon@fi.uba.ar>
2021-01-28 15:35:43 -0300

    Se agregaron métodos para ataque y defensa

--- ElementalFuego.cpp
+++ ElementalFuego.cpp
@@ -13,3 +13,45 @@ void ElementalFuego::alimentar() {
         energia += AUMENTO_ENERGIA_FUEGO;
     }
 }
+
+void ElementalFuego::atacar(Personaje* enemigos[MAX_PERSONAJES]) {
+    cout << "ATAQUE DE ELEMENTAL DE FUEGO" << endl;
+    if (this->energia >= ENERGIA_ATAQUE_FUEGO) {
+        for (int i = 0; i < MAX_PERSONAJES; i++) {
+            if (abs(enemigos[i]->getFila() - this->fila) <= 1) {
+                float danio = DANIO_FUEGO;
+                if (enemigos[i]->getElemento() == ELEMENTO_AIRE) {
+                    danio += MODIFICADOR_FUEGO;
+                }
+                else if (enemigos[i]->getElemento() == ELEMENTO_AGUA) {
+                    danio -= MODIFICADOR_FUEGO;
+                }
+                if (enemigos[i]->getEscudo() == 1) {
+                    danio = danio * 0.9;
+                }
+                else if (enemigos[i]->getEscudo() == 2) {
+                    danio = danio * 0.8;
+                }
+                else if (enemigos[i]->getEscudo() > 2) {
+                    danio = danio * 0.2;
+                }
+                enemigos[i]->setVida(enemigos[i]->getVida() - danio);
+            }
+        }
+        this->energia -= ENERGIA_ATAQUE_FUEGO;
+    }
+    else {
+        cout << "\tNo tiene energia suficiente para atacar." << endl;
+    }
+}
+
+void ElementalFuego::defender(Personaje* aliados[MAX_PERSONAJES]) {
+    cout << "DEFENSA DE ELEMENTAL DE FUEGO" << endl;
+    if (this->energia >= ENERGIA_DEFENSA_FUEGO) {
+        this->vida += RECUPERACION_FUEGO;
+        this->energia -= ENERGIA_DEFENSA_FUEGO;
+    }
+    else {
+        cout << "\tNo tiene energia suficiente para atacar." << endl;
+    }
+}
--- ElementalFuego.h
+++ ElementalFuego.h
@@ -6,7 +6,12 @@
 
 using namespace std;
 
-const int AUMENTO_ENERGIA_FUEGO = 15;
+#define AUMENTO_ENERGIA_FUEGO 15
+#define ENERGIA_ATAQUE_FUEGO 5
+#define DANIO_FUEGO 20
+#define MODIFICADOR_FUEGO 10
+#define ENERGIA_DEFENSA_FUEGO 10
+#define RECUPERACION_FUEGO 10
 
 class ElementalFuego: public Personaje{
 public:
@@ -17,6 +22,10 @@ public:
     // PRE: -
     // POS: Notifica al usuario cómo fue alimentado el Personaje y aumenta la energía en AUMENTO_ENERGIA_FUEGO
     void alimentar();
+    
+    void atacar(Personaje* enemigos[MAX_PERSONAJES]);
+    
+    void defender(Personaje* aliados[MAX_PERSONAJES]);
 };
 
 
