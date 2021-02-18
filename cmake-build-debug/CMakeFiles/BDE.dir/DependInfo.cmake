51e81bc01168008309a4692b3a4494420e32cc2d
edusosa24 <59883429+edusosa24@users.noreply.github.com>
2021-02-18 15:11:10 -0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/BDE.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/esosa/Desktop/BDE/ABB.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/ABB.cpp.o"
+  "/home/esosa/Desktop/BDE/ArchivoPersonajes.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/ArchivoPersonajes.cpp.o"
+  "/home/esosa/Desktop/BDE/Casillero.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/Casillero.cpp.o"
+  "/home/esosa/Desktop/BDE/ColaPrioridad.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/ColaPrioridad.cpp.o"
+  "/home/esosa/Desktop/BDE/DatosGrafo.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/DatosGrafo.cpp.o"
+  "/home/esosa/Desktop/BDE/ElementalAgua.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/ElementalAgua.cpp.o"
+  "/home/esosa/Desktop/BDE/ElementalAire.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/ElementalAire.cpp.o"
+  "/home/esosa/Desktop/BDE/ElementalFuego.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/ElementalFuego.cpp.o"
+  "/home/esosa/Desktop/BDE/ElementalTierra.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/ElementalTierra.cpp.o"
+  "/home/esosa/Desktop/BDE/Etiqueta.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/Etiqueta.cpp.o"
+  "/home/esosa/Desktop/BDE/Grafo.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/Grafo.cpp.o"
+  "/home/esosa/Desktop/BDE/Juego.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/Juego.cpp.o"
+  "/home/esosa/Desktop/BDE/Jugador.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/Jugador.cpp.o"
+  "/home/esosa/Desktop/BDE/Jugar.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/Jugar.cpp.o"
+  "/home/esosa/Desktop/BDE/MenuInicio.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/MenuInicio.cpp.o"
+  "/home/esosa/Desktop/BDE/MenuJuego.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/MenuJuego.cpp.o"
+  "/home/esosa/Desktop/BDE/MenuSeleccion.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/MenuSeleccion.cpp.o"
+  "/home/esosa/Desktop/BDE/NodoABB.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/NodoABB.cpp.o"
+  "/home/esosa/Desktop/BDE/NodoColaPrioridad.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/NodoColaPrioridad.cpp.o"
+  "/home/esosa/Desktop/BDE/Personaje.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/Personaje.cpp.o"
+  "/home/esosa/Desktop/BDE/Tablero.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/Tablero.cpp.o"
+  "/home/esosa/Desktop/BDE/Utiles.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/Utiles.cpp.o"
+  "/home/esosa/Desktop/BDE/main.cpp" "/home/esosa/Desktop/BDE/cmake-build-debug/CMakeFiles/BDE.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
